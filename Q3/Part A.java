95b6d265e65444464bb012da4cae5cb7aa9d7f8a
sherzod1221 <sherzods2@nycstudents.net>
2025-05-21 11:35:35 -0400

    Update Part A.java

--- Q3/Part A.java
+++ Q3/Part A.java
@@ -1,1 +1,8 @@
+public Round(String[] names) {
+    competitorList = new ArrayList<Competitor>();
 
+    for (int i = 0; i<names.length; i++){
+        Competitor c = new Competitor(names[i],i+1);
+        competitorList.add(c);
+    }
+}
