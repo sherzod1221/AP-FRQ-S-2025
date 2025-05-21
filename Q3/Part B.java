a0a29da1b9488240ec02a00c36bae60259f00a20
sherzod1221 <sherzods2@nycstudents.net>
2025-05-21 11:38:45 -0400

    Update Part B.java

--- Q3/Part B.java
+++ Q3/Part B.java
@@ -1,1 +1,20 @@
+public ArrayList<Match> buildMatches() {
+    ArrayList<Match> = new ArrayList<Match>();
+    int start = 0;
+    int end = competitorList.size() - 1;
+
+    if (competitorList.size() %2 != 0) {
+        start = 1;
+    }
+
+    while (start < end) {
+        Competitor c1 = competitorList.get(start);
+        Competitor c2 = competitorList.get(end);
+        matches.add(new Match(c1,c2));
+        start++;
+        end--;
+    }
+
+    return matches;
+}
 
